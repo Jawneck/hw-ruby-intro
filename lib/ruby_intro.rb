b6729699a4dd235529743005681bde123da4f591
EC2 Default User <ec2-user@ip-172-31-46-229.eu-west-1.compute.internal>
2019-03-10 11:40:30 +0000

    adding work from cloud9 to git hub

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,35 +1,62 @@
-# When done, submit this entire file to the autograder.
-
 # Part 1
-
-def sum arr
-  # YOUR CODE HERE
+def sum(a)
+	s = 0
+	a.each {|x| s+= x}
+	s
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum(a)
+	return 0 if a.empty?
+	return a[0] if a.length == 1
+	ar = a.sort
+	ar[-1] + ar[-2]
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n?(a, n)
+	return false if a.empty? or a.length == 1
+
+	h = Hash.new
+	a.each{|x| 
+		if h.key? x
+			return true
+		else
+			h[n - x] = n	
+		end
+	}
+	return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+	"Hello, #{name}"
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+
+def starts_with_consonant? (s)
+	/^[b-df-hj-np-tv-z]/i.match(s) != nil
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
+def binary_multiple_of_4? (s)
+	return true if s == "0"
+	/^[10]*00$/.match(s) != nil
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+
+	attr_accessor :isbn
+	attr_accessor :price
+
+	def initialize(isbn, price)
+		raise ArgumentError, 
+			"Illegal argument" if isbn.empty?  or price <= 0
+		@isbn = isbn
+		@price = price
+	end
+
+	def price_as_string
+		sprintf("$%2.2f", @price)
+	end
 end
